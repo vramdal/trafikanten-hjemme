2522ff8655fbab8294ca148b77fa2ad5b6b3fc21
Vidar S. Ramdal <vidar.ramdal@webstep.no>
2015-01-29 00:45:20 +0100

    More concatenator tests

--- test/concatenator-spec.js
+++ test/concatenator-spec.js
@@ -5,9 +5,23 @@ var concatenatorTest = require("./RedTest.js")(concatenator);
 
 describe("concatenator", function() {
     it("should concatenate two strings", function(done) {
-        var concatenator = concatenatorTest({"forceArray": true});
+        var concatenator = concatenatorTest();
         concatenator.trigger("input", {"payload": ["some random string 1", "some random string 2"]}, function(msg) {
             assert.equal("some random string 1" + concatenator.glue + "some random string 2", msg.payload);
+            done();
+        });
+    });
+    it("should not join on only one string", function(done) {
+        var concatenator = concatenatorTest();
+        concatenator.trigger("input", {"payload": ["some random string 1"]}, function(msg) {
+            assert.equal("some random string 1", msg.payload);
+            done();
+        });
+    });
+    it("should not do anything if payload is not an array", function(done) {
+        var concatenator = concatenatorTest();
+        concatenator.trigger("input", {"payload": "some random string 1"}, function(msg) {
+            assert.equal("some random string 1", msg.payload);
             done();
         });
     });
