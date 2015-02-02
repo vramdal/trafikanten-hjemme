01603bb44f7593ce8f79f2e72552a298a9773edb
Vidar S. Ramdal <vidar.ramdal@webstep.no>
2015-02-02 01:43:32 +0100

    Removing ofe

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ var request = require('request');
 
 
 var app = express();
+/*
 if (app.get('env') === 'development') {
   require('ofe').call(); // Uncomment to enable heap dumps on
 }
+*/
 
 app.use(logger('dev'));
 app.use(favicon(__dirname + '/public/images/favicon.gif'));
