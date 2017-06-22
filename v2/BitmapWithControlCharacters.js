4966e6b1b9d5e7c91f945eeda7e6860eaf4b1bb6
Vidar Ramdal <Vidar.Ramdal@norgesgruppen.no>
2017-06-22 15:15:32 +0200

    Bitmap clip

--- v2/BitmapWithControlCharacters.js
+++ v2/BitmapWithControlCharacters.js
@@ -12,16 +12,32 @@ module.exports = class BitmapWithControlCharacters {
 
     _bitmap : Bitmap;
     _renderControls : RenderControlMap;
+    _clip : number;
 
     /**
      * @param bitmap An array of bytes, each byte corresponding to a column of the bitmap.
      * @param renderControls An array where control characters are placed. The array's length should equal size of
      * {@link bitmap}. If a control character should be placed in front of
      * byte 6, the control character should be placed at <code>controlCharacters[6]</code>.
+     * @param clip Optional. If set to a number, it specifies that anything to the right of <code>graphicLength</code> should
+     * be considered not used (blank). If <code>false</code>, will use the width of {@link bitmap}.
+     * If not set, the value will be calculated.
      */
-    constructor(bitmap: Bitmap, renderControls: RenderControlMap = []) {
+    constructor(bitmap: Bitmap, renderControls: RenderControlMap = [], clip : ?number | boolean) {
         this._bitmap = bitmap;
         this._renderControls = renderControls;
+        if (typeof clip === 'number') {
+            this._clip = clip;
+        } else if (clip === false) {
+            this._clip = this._bitmap.length;
+        } else {
+            this._clip = 0;
+            for (let i = 0; i < this._bitmap.length; i++) {
+                if (this._bitmap[i] !== 0) {
+                    this._clip = i + 1;
+                }
+            }
+        }
     }
 
     get bitmap() : Bitmap {
@@ -30,5 +46,9 @@ module.exports = class BitmapWithControlCharacters {
 
     get renderControls() : RenderControlMap {
         return this._renderControls;
+    }
+
+    get clip(): number {
+        return this._clip;
     }
 };
