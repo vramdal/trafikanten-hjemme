1d654d70a2295fe2bb9696c4af92a9f4252d23b0
Vidar S. Ramdal <vramdal@gmail.com>
2017-06-25 15:50:01 +0200

    Implemented Frame as Proxy

--- v2/Frame.js
+++ v2/Frame.js
@@ -1,6 +1,7 @@
 // @flow
 
 import type {Bitmap} from "./BitmapWithControlCharacters";
+
 class Frame {
     _width : number;
     _scrollOffset: number;
@@ -11,18 +12,45 @@ class Frame {
         this._width = width;
         this._source = source;
         this._scrollOffset = 0;
-        this._updateView();
+        this._bitmap = new Proxy(new Uint8Array(this._source.buffer, this._source.byteOffset, this._source.length), {
+            get: function(target, propertyKey) {
+                let idx;
+                if (typeof propertyKey === "string" && !isNaN(parseInt(propertyKey))) {
+                    idx = parseInt(propertyKey, 10);
+                } else if (typeof propertyKey === "number") {
+                    idx = propertyKey;
+                }
+                if (idx !== undefined) {
+                    return this.getByIdx(idx);
+                } else {
+                    return Reflect.get(target, propertyKey, target);
+                }
+            },
+            getByIdx: function(idx) {
+                return _this._getAdjustedByScrollOffset(idx);
+            }
+        });
+        let _this = this;
+    }
+
+    _getAdjustedByScrollOffset(idx : number) {
+        let max = this._width;
+        let offsetIdx = idx - this._scrollOffset;
+        if (idx < 0) {
+            throw new RangeError("Out of range: " + idx);
+        } else if (idx > max) {
+            throw new RangeError(`Out of range: ${idx}, max is ${max}`);
+        } else if (offsetIdx < 0) {
+            return 0;
+        } else if (offsetIdx > this._width) {
+            return 0;
+        } else {
+            return this._source[offsetIdx];
+        }
     }
 
     scroll(delta : number) {
         this._scrollOffset += delta;
-        this._updateView();
-    }
-
-    _updateView() {
-        let start = this._source.byteOffset - this._scrollOffset;
-        // TODO: What to do when scrolling out of buffer;
-        this._bitmap = new Uint8Array(this._source.buffer, start, this._width);
     }
 
     get bitmap(): Bitmap {
