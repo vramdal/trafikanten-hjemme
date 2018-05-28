28a51749eed7b5bdb2a3a11769dc1ece1f2bbe00
Vidar Ramdal <vidar.ramdal@sparebank1.no>
2018-05-28 14:23:02 +0200

    More lenient address parsing

--- v2/geocoding/EnturGeocoder.js
+++ v2/geocoding/EnturGeocoder.js
@@ -13,17 +13,30 @@ class EnturGeocoder implements Geocoder {
     fetcher: CachedFetcher<string, Coordinates>;
 
     constructor() {
-        this.fetcher = new CachedFetcher({}, (placeStr) => {
+        let fetcher = new CachedFetcher({}, (placeStr) => {
             const url = `https://api.entur.org/api/geocoder/1.1/search?text=${encodeURIComponent(placeStr)}&categories=NO_FILTER&focus.point.lat=59.91&focus.point.lon=10.76&lang=en`;
             return fetch(url, headers)
                 .then(res => res.json())
                 .then(json => {
-                    let coordinates = json.features.slice(0, 1)
-                        .map(feature => feature.geometry)
-                        .map(geometry => ({lat: geometry.coordinates[0], long: geometry.coordinates[1]}));
-                    return coordinates[0] || null;
+                    if (json.features.length === 0
+                        && json.geocoding
+                        && json.geocoding.query
+                        && json.geocoding.query.parsed_text
+                        && json.geocoding.query.parsed_text.admin_parts) {
+                        console.info(`No match for "${placeStr}", trying "${json.geocoding.query.parsed_text.admin_parts}"`);
+                        return fetcher.getValue(json.geocoding.query.parsed_text.admin_parts);
+                    } else if (json.features.length === 0) {
+                        console.info(`No match for "${placeStr}", cannot geocode"`);
+                        return null;
+                    } else {
+                        let coordinates = json.features.slice(0, 1)
+                            .map(feature => feature.geometry)
+                            .map(geometry => ({lat: geometry.coordinates[0], long: geometry.coordinates[1]}));
+                        return coordinates[0] || null;
+                    }
                 });
-        })
+        });
+        this.fetcher = fetcher
     }
 
     getCoordinates(str : string) {
