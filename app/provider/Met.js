62f12937ed380d67b5332cc311dbfa4da4fa4286
Vidar S. Ramdal <vramdal@gmail.com>
2018-09-11 23:49:54 +0200

    fixup! Removing default title

--- app/provider/Met.js
+++ app/provider/Met.js
@@ -143,7 +143,7 @@ class Met implements PlaylistProvider {
     //noinspection JSUnusedLocalSymbols
     constructor(id : string, dataStore : PreemptiveCache, location : LatLong, title : ?string) {
         this._id = `Met:${title || location}`;
-        this.title = title; //? title +  "\nfra met.no" : "Værvarsel\nfra met.no";
+        this.title = title;
 
         this._forecastFetcher = new ValueFetcherAndFormatter(
             `${this._id}-forecast`,
